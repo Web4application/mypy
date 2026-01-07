3c09dac02637dec7df2020261dd06bb1c9cf9bd3
Seriki yakub <sandraisom00@gmail.com>
2026-01-07 23:35:09 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,7 @@
 git clone https://github.com/python/mypy.git --recurse-submodules
 git -C mypy checkout $(cat mypy_commit)
 pipx run cibuildwheel --config=cibuildwheel.toml mypy
+
+brew install pipx
+pipx ensurepath
+sudo pipx ensurepath --global # optional to allow pipx actions with --global argument
